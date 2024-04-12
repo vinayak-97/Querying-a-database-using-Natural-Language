a88b759f48b81b1ba7f00c8b75a8c1d3ec41f651
Vinayak Bhosale <61471717+vinayak-97@users.noreply.github.com>
2024-04-12 19:22:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("AtliQ T Shirts: Database Q&A 👕")
+st.title("Querying a DataBase using Natural Language 🤖")
 
 question = st.text_input("Question: ")
 
